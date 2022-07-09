16e5f398313046f8285a1173ee25650340765d04
patricknikolaus <pdn6991@gmail.com>
2022-07-09 01:10:49 -0400

    seeds v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 3.times do
+#   user_name = Faker::Color.color_name + Faker::Creature::Animal.name
+#   user_email = Faker::Internet.email
+#   User.create(username: "#{user_name}", email: "#{user_email}")
+# end
+
+# 10.times do
+#   recipe_user_id = rand(1..3)
+#   recipe_name = Faker::Food.dish
+#   recipe_chef = Faker::Name.name
+#   p recipe_chef
+#   p recipe_user_id
+#   p recipe_name
+#   Recipe.create(user_id: recipe_user_id, name: "#{recipe_name}", chef: "#{recipe_chef}")
+# end
+
+# 25.times do
+#   food = Faker::Food.ingredient
+#   num = rand(1..3)
+#   i = rand(0..2)
+#   r_id = rand(3..12)
+#   measure = ["Tbsp", "tsp", "cup", "oz", "g"]
+#   Ingredient.create(name: "#{food}", measurement: "#{num} #{measure[i]}", recipe_id: r_id)
+# end
