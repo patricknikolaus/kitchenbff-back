019f02754589ab3046fe06d4d6b1cd61f77d5dc1
fellowbeard <virgovibe@gmail.com>
2022-07-11 12:46:59 -1000

    adds actions, routes, requests to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,37 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes
+  end
+
+  def create
+    recipe = Recipe.new(
+      title: params[:user_id],
+      description: params[:description],
+      prep_time: params[:prep_time],
+      cook_time: params[:cook_time],
+    )
+    recipe.save
+    render json: recipe
+  end
+
+  def show
+    recipe = Recipe.find_by(id: params[:id])
+    render json: recipe
+
+  def update
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.title = params[:title] || recipe.title
+    recipe.description = params[:description] || recipe.description
+    recipe.prep_time = params[:prep_time] || recipe.prep_time
+    recipe.cook_time = params[:cook_time] || recipe.cook_time
+    recipe.save
+    render json: recipe
+  end
+
+  def destroy
+    favorite = Favorite.find_by(id: params[:id])
+    favorite.destroy
+    render json: {message: "Favorite successfully deleted."}
+  end
 end
