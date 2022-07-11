cec7f4b34fb3b7fdaeaeb18971bf8d64ca905b06
fellowbeard <virgovibe@gmail.com>
2022-07-11 12:14:18 -1000

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   ###Ingredients Routes
   get "/ingredients" => "ingredients#index"
   post "/ingredients" => "ingredients#create"
+  get "/ingredients/:id" => "ingredients#show"
   delete "/ingredients/:id" => "ingredients#destroy"
 end
