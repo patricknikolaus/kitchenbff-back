751a945b251a928b25d66a3b9da4030670d0b514
fellowbeard <virgovibe@gmail.com>
2022-07-16 11:34:34 -1000

    adds recipe controller, routes, requests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,40 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes
+  end
+
+  def create
+    recipe = Recipe.new(
+      title: params[:title],
+      prep_time: params[:prep_time],
+      cook_time: params[:cook_time],
+      description: params[:description]      
+    )
+    recipe.save
+    render json: recipe
 end
+
+def show
+  recipe = Recipe.find_by(id: params[:id])
+  render json: recipe
+end
+
+def update
+  recipe = Recipe.find_by(id: params[:id])
+  recipe.prep_time = params[:prep_time] || recipe.prep_time
+  recipe.title = params[:title] || recipe.title
+  recipe.cook_time = params[:cook_time] || recipe.cook_time
+  recipe.description = params[:description] || recipe.description
+  recipe.save
+  render json: recipe
+end
+
+  def destroy
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.destroy
+    render json: {message: "Recipe successfully deleted."}
+  end
+end
+
+
