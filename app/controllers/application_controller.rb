7f25719d754816d02482006a36524b71d720388b
patricknikolaus <pdn6991@gmail.com>
2022-07-17 22:09:49 -0400

    fixed typo in current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-  auth_headers = request.headers["Authorization"]
+  def current_user
+    auth_headers = request.headers["Authorization"]
     if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
       token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
       begin
